30795711af01d8c546f43b4eaddf5b80f0c4a526
Skycader <skycader@yandex.ru>
2026-03-10 10:59:59 +0300

    OK

---
+++ bots/index.js
@@ -0,0 +1,13 @@
+// bots/index.js — список всех ботов для игры
+// Добавь сюда свой класс чтобы он появился в игре
+
+// Порядок = порядок спавна
+const botClasses = [
+  Dummy,
+  // Attacker,
+  // Crazy,
+  // Shy,
+  // Coward,
+  // Curious,
+  // Scanner,
+];
